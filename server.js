a9feac5605a1e71f63618ec115d76182809c49fa
hanmiton <hanmilton_12@outlook.com>
2017-04-12 18:49:18 -0500

    instalacion y coneccion a mongodb

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ var express = require('express')
 var app = express()
 var port = process.env.PORT || 80
 var morgan = require('morgan')
+var mongoose = require('mongoose')
 
 app.use(morgan('dev'))
+
+mongoose.connect('mongodb://localhost:27017/tutorial', function(err){
+	if(err){
+		console.log('Not Connected to the database: ' + err)
+	}
+	else{
+		console.log('Successfully connected to MongoDB')
+	}
+})
 
 app.listen(port, function(){
 	console.log('Running the server on port' + port)
