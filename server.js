7b6e77c6700db9817bd2b4d120c417ce0053dbe1
hanmiton <hanmilton_12@outlook.com>
2017-04-12 14:15:25 -0500

    cambio para probar pull desde el servidor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.get('/',function(req,res){
-	res.send('Unidad de gestion deployment')
+	res.send('Unidad de gestion deployment probando pull')
 })
 
 app.listen(process.env.PORT || 80, function(){
