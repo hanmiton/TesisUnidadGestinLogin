e209d235d4348d2dcc1becc136a1f481b68c5835
hanmiton <hanmilton_12@outlook.com>
2017-04-19 00:56:39 -0500

    restringiendo rutas

--- public/app/routes.js
+++ public/app/routes.js
@@ -1,4 +1,4 @@
-angular.module('appRoutes', ['ngRoute'])
+var app = angular.module('appRoutes', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider){
 	$routeProvider
@@ -14,55 +14,65 @@ angular.module('appRoutes', ['ngRoute'])
 	.when('/register', {
 		templateUrl: 'app/views/pages/users/register.html',
 		controller: 'regCtrl',
-		controllerAs: 'register'
+		controllerAs: 'register',
+		authenticated : false
 	})
 	
 	.when('/login',{
-		templateUrl: 'app/views/pages/users/login.html'
+		templateUrl: 'app/views/pages/users/login.html',
+		authenticated : false
 	})
 
 	.when('/logout',{
-		templateUrl: 'app/views/pages/users/logout.html'
+		templateUrl: 'app/views/pages/users/logout.html',
+		authenticated : true
 	})
 
 	.when('/profile', {
-		templateUrl: 'app/views/pages/users/profile.html'
+		templateUrl: 'app/views/pages/users/profile.html',
+		authenticated : true
 	})
 
 	.when('/facebook/:token', {
 		templateUrl: 'app/views/pages/users/social/social.html',
 		controller: 'facebookCtrl',
-		controllerAs: 'facebook'
+		controllerAs: 'facebook',
+		authenticated : false
 	})
 
 	.when('/twitter/:token', {
 		templateUrl: 'app/views/pages/users/social/social.html',
 		controller: 'twitterCtrl',
-		controllerAs: 'twitter'
+		controllerAs: 'twitter',
+		authenticated : false
 	})
 
 	.when('/google/:token', {
 		templateUrl: 'app/views/pages/users/social/social.html',
 		controller: 'googleCtrl',
-		controllerAs: 'google'
+		controllerAs: 'google',
+		authenticated : false
 	})
 
 	.when('/facebookerror', {
 		templateUrl: 'app/views/pages/users/login.html',
 		controller: 'facebookCtrl',
-		controllerAs: 'facebook'
+		controllerAs: 'facebook',
+		authenticated : false
 	})
 
 	.when('/twittererror', {
 		templateUrl: 'app/views/pages/users/login.html',
 		controller: 'twitterCtrl',
-		controllerAs: 'twitter'
+		controllerAs: 'twitter',
+		authenticated : false
 	})
 
 	.when('/googleerror', {
 		templateUrl: 'app/views/pages/users/login.html',
 		controller: 'googleCtrl',
-		controllerAs: 'google'
+		controllerAs: 'google',
+		authenticated : false
 	})
 
 	.otherwise( {redirectTo: '/'});
@@ -73,3 +83,24 @@ angular.module('appRoutes', ['ngRoute'])
 	});
 
 }); 
+
+app.run(['$rootScope', 'Auth' , '$location' , function($rootScope, Auth, $location) {
+
+	$rootScope.$on('$routeChangeStart', function(event, next, current){
+		
+		if(next.$$route.authenticated == true) {
+			//console.log('needs to be authenticated');
+			//importante rutas para manejo de modulo recepcion financioeron y seguimiento
+			if (!Auth.isLoggedIn()) {
+				event.preventDefault();
+				$location.path('/');
+			}
+		}else if( next.$$route.authenticated == false) {
+			//console.log('should not be authenticated');
+			if (Auth.isLoggedIn()) {
+				event.preventDefault();
+				$location.path('/profile');
+			}
+		} 
+	});
+}]);
